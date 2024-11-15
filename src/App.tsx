1a5a7dd5708d6ed906e7f8ec53a88417a6b2d94c
dothanhtien <37395677+dothanhtien@users.noreply.github.com>
2024-11-15 21:07:27 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn CD with Github Actions
         </a>
       </header>
     </div>
